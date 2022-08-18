a9f7c9efb508aa1a4085ab57b5c5ae22fed7e44f
Dawson Hensel <dawsonhensel@gmail.com>
2022-08-17 18:38:57 -0700

    add broken hash table, anagrams, and main func.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 
 import itertools
+import json
 
 def permutate(letters):
     string_permutations = itertools.permutations(letters)
@@ -8,3 +9,34 @@ def permutate(letters):
     string_permutations = [''.join(permutation) for permutation in string_permutations]
 
     return string_permutations
+
+def buildHashTable():
+    with open('dictionaries/words_dictionary.json', 'r') as file:
+        return json.load(file)
+
+
+def getValidAnagrams(permutations):
+    validAnagrams = {}
+
+    for permutation in permutations:
+        # "a fly dove" -> "a", "fly", "dove"
+        splitPermutation = permutation.split(" ")
+        for word in splitPermutation:
+
+            # Check if current word is a valid word from our dictionary.
+            if (dictionary[word] != "1"):
+                break
+
+            # Add to valid anagrams.
+            validAnagrams[permutation] = 1
+
+    return validAnagrams    
+
+if __name__=="__main__":
+    permutations = permutate("assess ")
+    dictionary = buildHashTable()
+    validAnagramsDict = getValidAnagrams(permutations)
+
+    for anagram in validAnagramsDict.keys():
+        print(anagram + "\n")
+
